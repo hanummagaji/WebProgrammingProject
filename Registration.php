b3dc34b8a86d422ba73e0d1958dcf271f9f89801
hanummagaji <119705847+hanummagaji@users.noreply.github.com>
2022-12-02 17:06:47 -0500

    Add files via upload

---
+++ Registration.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title> Register</title>
+	<link rel="stylesheet" type="text/css" href="Register.css">
+</head>
+<body>
+	<div class="signup-box">
+		<h1>Register</h1>
+		<form>
+			<label>Artist Name</label>
+			<input type="text" placeholder=" ">
+
+			<label>Art Type</label>
+			<input type="text" placeholder=" ">
+
+			<label>Email</label>
+			<input type="email" placeholder="">
+
+			<label> Username</label>
+			<input type="text" placeholder="">
+
+			<label>Password</label>
+			<input type="Password" placeholder="">
+			<input type="button" value="Submit">
+		</form>
+
+	</body>
